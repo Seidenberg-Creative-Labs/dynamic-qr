a5dfc511bee49dc9d81ad4e2e5a1512461f46f2e
L33t-Rose <jphillipjean@gmail.com>
2023-08-24 10:58:06 -0400

    * Created /r route * This will take in just the id in the firebase to redirect

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ print(BASE_URL)
 def test():
     return {"message":"Hello World"}
 
-@app.get("/{campus}/{room}")
+@app.get("/test/{campus}/{room}")
 def process(campus:str, room:str):
     slug = f"/{campus.upper()}/{room}"
     URL = BASE_URL+f'?orderBy="slug"&equalTo="{slug}"'
@@ -25,3 +25,14 @@ def process(campus:str, room:str):
         return RedirectResponse(obj[1]['redirectTo'])
     except:
         return PlainTextResponse('Does not exist',status_code=404)
+    
+
+@app.get("/r/{id}")
+def process(id:str):
+    print('id '+id)
+    URL = f"{BASE_URL}/{id}.json"
+    req = requests.get(URL)
+    data = req.json()
+    if data == None:
+        return PlainTextResponse('Does not exist',status_code=404)
+    return RedirectResponse(data['redirectTo'])
